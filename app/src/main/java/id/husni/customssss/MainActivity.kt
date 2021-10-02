1f1029c683b1bb3a77100491fe7f9a25c3913868
whoishusni <mohhusnimubaraq@gmail.com>
2021-10-02 14:16:07 +0800

    update

---
+++ app/src/main/java/id/husni/customssss/MainActivity.kt
@@ -0,0 +1,17 @@
+package id.husni.customssss
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
